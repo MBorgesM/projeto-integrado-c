1454ad1bda3794534d25fa1bba62538536277640
MBorgesM <matheusborgesmoraes1@gmail.com>
2021-05-11 22:31:16 -0300

    Arquivos base do banco de dados adicionados

---
+++ src/bd/dbos/LabirintoDBO.java
@@ -0,0 +1,9 @@
+package bd.dbos;
+
+public class LabirintoDBO {
+    private String nome;
+    private String dataCriacao;
+    private String dataUltimaModificacao;
+    private String conteudo;
+    private String criador;
+}
